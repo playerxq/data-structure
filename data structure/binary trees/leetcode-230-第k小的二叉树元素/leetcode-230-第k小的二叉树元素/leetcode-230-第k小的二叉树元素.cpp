09b6bc87ac54887f8a7d79902aeb0a3faf53f084
playerxq <1062842783@qq.com>
2017-01-11 10:48:49 +0800

    leetcode-230
    leetcode-230

---
+++ data structure/binary trees/leetcode-230-第k小的二叉树元素/leetcode-230-第k小的二叉树元素/leetcode-230-第k小的二叉树元素.cpp
@@ -0,0 +1,49 @@
+// leetcode-230-第k小的二叉树元素.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include<iomanip>
+#include <ctype.h>
+#include <algorithm>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode *> st;
+        TreeNode *p = root;
+        while(p || !st.empty())
+        {
+            while(p)
+            {
+                st.push(p);
+                p = p->left;
+            }
+            p = st.top();
+            if(--k == 0)
+                return p->val;
+            st.pop();
+            p = p->right;
+        }
+    }
+};
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+}
+
