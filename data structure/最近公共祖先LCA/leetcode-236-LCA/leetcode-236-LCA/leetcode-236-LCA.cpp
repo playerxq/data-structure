c1ea688dac20358dfb233cc0a67b8ee76c83d1ab
playerxq <1062842783@qq.com>
2017-01-13 10:51:36 +0800

    leetcode-236
    leetcode-236

---
+++ data structure/最近公共祖先LCA/leetcode-236-LCA/leetcode-236-LCA/leetcode-236-LCA.cpp
@@ -0,0 +1,34 @@
+// leetcode-236-LCA.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include<iomanip>
+#include <ctype.h>
+#include <algorithm>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (!root || root == p || root == q) return root;
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		return !left ? right : !right ? left : root;
+	}
+};
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+}
+
